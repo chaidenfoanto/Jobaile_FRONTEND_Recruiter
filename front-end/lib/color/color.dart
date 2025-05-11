3209ff5f5cc3df4d731686c33c5ea1ee4bcece3d
FranklinJaya2006 <148520978+FranklinJaya2006@users.noreply.github.com>
2025-05-12 00:04:39 +0800

    Login bang

---
+++ front-end/lib/color/color.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors {
+  static const primary = Color(0xFF0C3C81);
+  static const secondary = Color(0xFF898989);
+  static const background = Color(0xFFF5F5F5);
+  static const textDark = Color(0xFF212121);
+  static const textLight = Color(0xFF757575);
+  static const error = Color(0xFFD32F2F);
+}
