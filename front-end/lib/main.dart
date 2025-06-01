7687b7f1f3820b1cbb1e0f36eaae057095360df1
feliciawijaya2006 <148602986+feliciawijaya2006@users.noreply.github.com>
2025-06-01 23:14:34 +0800

    no update

--- front-end/lib/main.dart
+++ front-end/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:profile/views/pages/profile.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'cubit/navigation_cubit.dart';
+import 'views/navigation.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,11 +10,17 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Profile App',
-      home: ProfilePage(),
+      title: 'Job Finder App',
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(),
+        primarySwatch: Colors.blue,
+      ),
+      home: BlocProvider(
+        create: (_) => NavigationCubit(),
+        child: MainScreen(),
+      ),
     );
   }
 }
